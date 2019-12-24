708b850c51d9f8ec921e344096d8c141286c9190
aiden <cnk9109@gmail.com>
2019-12-24 20:55:33 +0900

    init spring study

---
+++ src/test/java/goozy/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package goozy.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
