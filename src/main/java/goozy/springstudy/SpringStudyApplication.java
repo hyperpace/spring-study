f41b367e2d190c750c73b0f0121931eaeeed53f5
aiden <cnk9109@gmail.com>
2019-12-31 11:18:26 +0900

    feat: @SpringBootApplication 사용

--- src/main/java/goozy/springstudy/SpringStudyApplication.java
+++ src/main/java/goozy/springstudy/SpringStudyApplication.java
@@ -1,21 +1,15 @@
 package goozy.springstudy;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Objects;
-
-
+// @Configuration, @ComponentScan 확장
+// 즉, 이클래스 자체가 빈 설정 파일이고 알아서 해준다.
+// SpringBoot가 아주 편하게 해준다.
+@SpringBootApplication
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanDefinitionNames));
-
-        BookService bookService = (BookService) applicationContext.getBean("bookService");
-        System.out.println(Objects.nonNull(bookService.getBookRepository()));
+        SpringApplication.run(SpringStudyApplication.class, args);
     }
 }
