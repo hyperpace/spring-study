9b91e08eecb811a656a30708b14fc22053e2ff29
aiden <cnk9109@gmail.com>
2019-12-31 11:12:53 +0900

    feat: @Configuration 클래스에 @ComponentScan 활용

--- src/main/java/goozy/springstudy/ApplicationConfig.java
+++ src/main/java/goozy/springstudy/ApplicationConfig.java
@@ -1,18 +1,12 @@
 package goozy.springstudy;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@ComponentScan(basePackages = "goozy.springstudy")
+@ComponentScan(basePackageClasses = SpringStudyApplication.class) // Type Safe한 방법
 public class ApplicationConfig {
 
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService() {
-        return new BookService();
-    }
 }
--- src/main/java/goozy/springstudy/BookService.java
+++ src/main/java/goozy/springstudy/BookService.java
@@ -1,7 +1,9 @@
 package goozy.springstudy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
     @Autowired
